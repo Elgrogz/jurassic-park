2ef167bc9ae7eb20359943e3388b5f1712dd3652
Elgrogz <gregorgilchrist@gmail.com>
2016-12-15 16:12:35 +0000

    properties added abstract class Dinosaur

---
+++ jurassic_park/Dinosaur.java
@@ -0,0 +1,16 @@
+abstract class Dinosaur {
+  String name;
+  String type;
+  int foodLevel;
+  int popularityLevel;
+
+  public Dinosaur(String name, String type, int popularityLevel) {
+    this.name = name;
+    this.type = type;
+    this.foodlevel = 5;
+    this.popularityLevel = popularityLevel;
+  }
+
+  
+  
+}
