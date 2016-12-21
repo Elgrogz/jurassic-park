8a1ddb96e3058b38e97247220c89beba05a47336
Elgrogz <gregorgilchrist@gmail.com>
2016-12-21 21:16:11 +0000

    final commit

--- jurassic_park/Enclosure.java
+++ jurassic_park/Enclosure.java
@@ -4,30 +4,19 @@ import java.util.*;
 public abstract class Enclosure {
 
   String name;
-  Boolean damaged;
 
   public Enclosure(String name) {
     this.name = name;
-    this.damaged = false;
   }
 
   public String getName() {
     return this.name;
   } 
 
-  public Boolean getDamageStatus() {
-    return this.damaged;
-  }
-
   public void setName(String name) {
     this.name = name;
   }
-  
-  public void damage() {
-    this.damaged = true;
-  }
 
-  // public abstract int getEnclosurePopularity();
   public abstract ArrayList<Dinosaur> returnDinosaurs();
   public abstract int getSize();
 
